278b8ff9a4f59ea719253a8a767e75a6db5adeaa
yangqibin-caibi <79695798+yangqibin-caibi@users.noreply.github.com>
2023-06-03 09:48:47 +0800

    Add files via upload

--- bullet.cpp
+++ bullet.cpp
@@ -1,14 +1,27 @@
 #include "bullet.h"
 
-Bullet::Bullet(QPointF _pos, qreal angle,int dura)
-    : pos(_pos), angle(angle),durability(dura)
+Bullet::Bullet(qreal x, qreal y, qreal angle,int dura)
+    : x(x), y(y), angle(angle),durability(dura)
 {
 //    setFlag(ItemIsMovable);
 //    setFlag(ItemIsSelectable);
 }
 
+QRectF Bullet::boundingRect() const
+{
+    return QRectF(-1, -1, 2, 2); // 设置子弹的包围矩形(不用管它）
+}
+
+void Bullet::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
+
+    painter->setBrush(Qt::black); // 设置子弹的颜色
+    //painter->drawEllipse(-1, -1, 2, 2); // 绘制子弹的圆形形状
+}
 QPointF Bullet::getPosition()const{
-return pos;
+    return QPointF(x,y);
 }
 int Bullet::getdurability()const{
     return durability;
@@ -28,14 +41,16 @@ void Bullet::setexist(bool f1){
     exist=f1;
 }
 void Bullet::collideWithWall(const Wall* wall){
-    if(wall->getRect().contains(pos))
-    {
-        angle= 2 * wall->getangle() - angle;
-        durability-=1;
-        if(durability<=0)exist=false;
+    QRectF rect=wall->getRect();
+    bool isOnLeftBoundary = (x == rect.left() && y >= rect.top() && y <= rect.bottom());
+    bool isOnTopBoundary = (y == rect.top() && x >= rect.left() && x <= rect.right());
+    bool isOnBottomBoundary = (y == rect.bottom() && x >= rect.left() && x <= rect.right());
+    bool isOnRightBoundary = (x == rect.right() && y >= rect.top() && y <= rect.bottom());
+    if(isOnLeftBoundary or isOnRightBoundary){
+        angle=2*90-angle;
     }
+    else if(isOnBottomBoundary or isOnTopBoundary){
+        angle=-angle;
+    }
+    return;
 }
-qreal getAngle()const{
-    return angle;
-}
-
